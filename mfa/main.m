0f0abf4bd96fe22e80a88bef3926e31dd9f5600d
xsdhy <402837074@qq.com>
2023-12-26 17:47:50 +0800

    Initial Commit

---
+++ mfa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mfa
+//
+//  Created by xsdhy on 2023/12/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
